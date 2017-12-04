e50eb364dc25b449640ad119181e878df1f7fc9f
Stanislaw Mazowiecki <stmazowiecki@gmail.com>
2017-12-04 21:33:14 +0100

    finished slider

--- js/script.jsx
+++ js/script.jsx
@@ -3,9 +3,25 @@ require('../sass/style.scss');
 $(document).ready(function(){
 
 
-
-
-
+    let swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        initialSlide: 1,
+        grabCursor: true,
+        pagination: '.swiper-pagination',
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 45,
+            depth: 60,
+            modifier: 2,
+            slideShadows: false,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 
 
 });

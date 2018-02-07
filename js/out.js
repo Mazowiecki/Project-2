7f1ab3cd2f64f85a53879e59c2258ec1c669976d
Stanisław Mazowiecki <stmazowiecki@gmail.com>
2018-02-07 20:50:13 +0100

    Update out.js

--- js/out.js
+++ js/out.js
@@ -98,7 +98,7 @@ $(document).ready(function () {
     
     ;(function($){
         $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
+            if (this.scrollTop() > 50) {
                 $('#back-to-top').fadeIn();
             } else {
                 $('#back-to-top').fadeOut();
